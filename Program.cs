bb3782d67a24d3e4f148b39be7690dcadb417eb0
Huiynia <114103139+Huiynia@users.noreply.github.com>
2022-10-13 00:47:03 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,92 +1,133 @@
-﻿namespace _3_задачи_2._0
+﻿using System.Linq.Expressions;
+using System.Security.AccessControl;
+
+namespace Практическая_работа_3._1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string operation;
+            Console.WriteLine("Нажмите Esc для выхода");
+            Console.WriteLine("Для перехода между первой и второй октавами нажмите f1 и f2 соответственно ");
+            octava_1_1();
+            return;
+            octava_2_2();
+            return;
+        }
+        static void octava_1_1()
+        {
+            Console.WriteLine("\nПервая октава");
+            int[] octava_1 = new int[12] { 261, 277, 293, 311, 329, 349, 370, 392, 415, 440, 466, 493 };
             do
             {
-                Console.WriteLine("Выберите программу, которую хотите запустить\n1. Угадай число\n2. Таблица умножения\n3. Вывод делителей числа\n4. Закрыть программу");
-                operation = Console.ReadLine();
-                if (operation == "1")
+                ConsoleKeyInfo Lox1 = Console.ReadKey();
+                switch (Lox1.Key)
                 {
-                    proga_1();
-                }
-                if (operation == "2")
-                {
-                    proga_2();
-                }
-                if (operation == "3")
-                {
-                    proga_3();
-                }
-                if (operation == "4")
-                {
-                    Console.WriteLine("Вы вышли из программы");
-                }
+                    case ConsoleKey.Q:
+                        Console.Beep(octava_1[0], 200);
+                        break;
+                    case ConsoleKey.W:
+                        Console.Beep(octava_1[1], 200);
+                        break;
+                    case ConsoleKey.E:
+                        Console.Beep(octava_1[2], 200);
+                        break;
+                    case ConsoleKey.R:
+                        Console.Beep(octava_1[3], 200);
+                        break;
+                    case ConsoleKey.T:
+                        Console.Beep(octava_1[4], 200);
+                        break;
+                    case ConsoleKey.Y:
+                        Console.Beep(octava_1[5], 200);
+                        break;
+                    case ConsoleKey.U:
+                        Console.Beep(octava_1[6], 200);
+                        break;
+                    case ConsoleKey.I:
+                        Console.Beep(octava_1[7], 200);
+                        break;
+                    case ConsoleKey.O:
+                        Console.Beep(octava_1[8], 200);
+                        break;
+                    case ConsoleKey.P:
+                        Console.Beep(octava_1[9], 200);
+                        break;
+                    case ConsoleKey.A:
+                        Console.Beep(octava_1[10], 200);
+                        break;
+                    case ConsoleKey.S:
+                        Console.Beep(octava_1[11], 200);
+                        break;
+                    case ConsoleKey.F2:
+                        switch_oct_2();
+                        return;
+                    case ConsoleKey.Escape:
+                        return;
 
-            } while (operation != "4");
+                }
+            } while (true);
         }
-        static void proga_1()
+        static void octava_2_2()
         {
-            Random rnd = new Random();
-            int value = rnd.Next(0, 100);
-            Console.WriteLine("Введи число");
-            int lucky = Convert.ToInt32(Console.ReadLine());
-            while (true)
-            {
-                if (value > lucky)
-                {
-                    Console.WriteLine("Больше");
-                    lucky = Convert.ToInt32(Console.ReadLine());
-                }
-                else if (value < lucky)
-                {
-                    Console.WriteLine("Меньше");
-                    lucky = Convert.ToInt32(Console.ReadLine());
-                }
-                if (value == lucky)
-                {
-                    Console.WriteLine("Вы угадали число");
-                    break;
-                }
-            }
-        }
-        static void proga_2()
-        {
-            Console.WriteLine("Таблица умножения");
-            int[,] matrica = new int[10, 1];
-            for (int j = 1; j < 10; j++)
-            {
-                for (int k = 1; k < 10; k++)
-                {
-                    Console.Write("\t" + j * k);
-                }
-                Console.WriteLine("\t");
-            }
-        }
-        static void proga_3()
-        {
-            string delimoe;
+            Console.WriteLine("\nВторая октава");
+            int[] octava_2 = new int[12] { 523, 554, 587, 622, 659, 698, 740, 784, 830, 880, 932, 987 };
             do
             {
-                Console.WriteLine("Введите делимое, для выхода из программы введите 'выйти'");
-                delimoe = Console.ReadLine();
-                if (delimoe == "выйти")
+                ConsoleKeyInfo Lox1 = Console.ReadKey();
+                switch (Lox1.Key)
                 {
-                    Console.WriteLine("Вы вышли в главное меню");
-                    break;
+                    case ConsoleKey.Q:
+                        Console.Beep(octava_2[0], 200);
+                        break;
+                    case ConsoleKey.W:
+                        Console.Beep(octava_2[1], 200);
+                        break;
+                    case ConsoleKey.E:
+                        Console.Beep(octava_2[2], 200);
+                        break;
+                    case ConsoleKey.R:
+                        Console.Beep(octava_2[3], 200);
+                        break;
+                    case ConsoleKey.T:
+                        Console.Beep(octava_2[4], 200);
+                        break;
+                    case ConsoleKey.Y:
+                        Console.Beep(octava_2[5], 200);
+                        break;
+                    case ConsoleKey.U:
+                        Console.Beep(octava_2[6], 200);
+                        break;
+                    case ConsoleKey.I:
+                        Console.Beep(octava_2[7], 200);
+                        break;
+                    case ConsoleKey.O:
+                        Console.Beep(octava_2[8], 200);
+                        break;
+                    case ConsoleKey.P:
+                        Console.Beep(octava_2[9], 200);
+                        break;
+                    case ConsoleKey.A:
+                        Console.Beep(octava_2[10], 200);
+                        break;
+                    case ConsoleKey.S:
+                        Console.Beep(octava_2[11], 200);
+                        break;
+                    case ConsoleKey.F1:
+                        switch_oct_1();
+                        return;
+                    case ConsoleKey.Escape:
+                        return;
                 }
-                for (int i = 1; i <= Convert.ToInt32(delimoe); i++)
-                {
-                    if (Convert.ToInt32(delimoe) % i == 0)
-                    {
-                        Console.WriteLine("Число {0} делится без остатка на {1}", delimoe, i);
-                    }
-                }
-            } while (delimoe != "выйти");
+            } while (true);
         }
-
+        static void switch_oct_1()
+        {
+            octava_1_1();
+        }
+        static void switch_oct_2()
+        {
+            octava_2_2();
+        }
     }
 }
