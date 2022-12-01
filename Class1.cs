e48ded2a0e14effdb82959f0987b06bb8f358881
da-da <114103139+Dada645@users.noreply.github.com>
2022-12-01 19:11:32 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class HH
+{
+    public string name;
+    public string value_0;
+    public string value_1;
+
+    public HH() { }
+    public HH(string name_0, string value_00, string value_11)
+    {
+        name = name_0;
+        value_0 = value_00;
+        value_1 = value_11;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Consoep7
+{
+    internal class Program
+    {
+        static public ConsoleKeyInfo da_da;
+        static void Main(string[] args)
+        {
+            Converter();
+        }
+        static void Converter()
+        {
+            string a;
+            string f;
+            HH cube_0 = new HH("Квадрат", "16", "16");
+            HH rectangle = new HH("Прямоугольник", "45", "26");
+            HH cube_1 = new HH("Квадрат", "21", "21");
+            List<HH> geo = new List<HH>();
+            geo.Add(cube_0);
+            geo.Add(rectangle);
+            geo.Add(cube_1);
+            string what = cube_0.name + "\n" + cube_0.value_0 + "\n" + cube_0.value_1 + "\n" + rectangle.name + "\n" + rectangle.value_0 + "\n" + rectangle.value_1 + "\n" + cube_1.name + "\n" + cube_1.value_0 + "\n" + cube_1.value_1;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Введите путь до файла (вместе с названием), который вы хотите открыть\n" + "--------------------------------------------------------------------------");
+                a = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("Сохранить файл в одном из трех форматов (txt, json, xml) - f1, выйти - Escape\n" + "--------------------------------------------------------------------------");
+                f = File.ReadAllText(a);
+                Console.WriteLine(f);
+                da_da = Console.ReadKey();
+                if (da_da.Key == ConsoleKey.F1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Введите путь до файла (вместе с названием), куда вы хотите сохранить текст\n" + "--------------------------------------------------------------------------");
+                    a = Console.ReadLine();
+                    if (a.EndsWith("n"))
+                    {
+                        JsonConvert.SerializeObject(what);
+                        File.WriteAllText(a, what);
+                    }
+                    if (a.EndsWith("l"))
+                    {
+                        XmlSerializer x = new XmlSerializer(typeof(HH));
+                        using (FileStream fs = new FileStream(a, FileMode.OpenOrCreate))
+                        {
+                            x.Serialize(fs, cube_0);
+                            x.Serialize(fs, rectangle);
+                            x.Serialize(fs, cube_1);
+                        }
+                    }
+                    if (a.EndsWith("t"))
+                    {
+                        File.WriteAllText(a, what);
+                    }
+                }
+            }while (da_da.Key != ConsoleKey.Escape);
+        }
+    }
+}
